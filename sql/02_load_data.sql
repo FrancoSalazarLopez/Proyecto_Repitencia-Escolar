11f6050431b8ce2c6fde38d9324f00b909f6aac3
Franco Salazar <francosalazarhr@gmail.com>
2026-03-13 15:57:49 -0300

    Update 02_load_data.sql

--- sql/02_load_data.sql
+++ sql/02_load_data.sql
@@ -1,3 +1,12 @@
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/stg_inversion.csv' 
+INTO TABLE stg_inversion
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\n' 
+IGNORE 1 ROWS
+(anio,mes,municipio_id,municipio_nombre,concepto,monto);
+-----------------------------------------------------------------------------------------------------------------------------------------------
+
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/stg_colegios.csv' 
 INTO TABLE stg_colegios
 FIELDS TERMINATED BY ',' 
@@ -7,13 +16,4 @@ IGNORE 1 ROWS
 (anio, municipio_id, municipio_nombre, unidades_de_servicio_nivel_inicial, unidades_de_servicio_nivel_primario,
 unidades_de_servicio_nivel_secundario, sector_de_gestión_estatal, sector_de_gestión_privada, matricula_sexo_femenino, 
 matricula_sexo_masculino, matricula_nivel_inicial, matricula_nivel_primario, matricula_nivel_secundario);
------------------------------------------------------------------------------------------------------------------------------------------------
-
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/stg_inversion.csv' 
-INTO TABLE stg_inversion
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"' 
-LINES TERMINATED BY '\n' 
-IGNORE 1 ROWS
-(anio,mes,municipio_id,municipio_nombre,concepto,monto);
 -----------------------------------------------------------------------------------------------------------------------------------------------
