102390043c01db87cf674fece08c0d3e41a094e4
Franco Salazar <francosalazarhr@gmail.com>
2026-03-13 15:56:23 -0300

    Update 02_load_data.sql

--- sql/02_load_data.sql
+++ sql/02_load_data.sql
@@ -1,5 +1,5 @@
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/stg_colegios.csv' 
-INTO TABLE stg_unga_unga
+INTO TABLE stg_colegios
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"' 
 LINES TERMINATED BY '\n' 
@@ -7,3 +7,13 @@ IGNORE 1 ROWS
 (anio, municipio_id, municipio_nombre, unidades_de_servicio_nivel_inicial, unidades_de_servicio_nivel_primario,
 unidades_de_servicio_nivel_secundario, sector_de_gestión_estatal, sector_de_gestión_privada, matricula_sexo_femenino, 
 matricula_sexo_masculino, matricula_nivel_inicial, matricula_nivel_primario, matricula_nivel_secundario);
+-----------------------------------------------------------------------------------------------------------------------------------------------
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/stg_inversion.csv' 
+INTO TABLE stg_inversion
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\n' 
+IGNORE 1 ROWS
+(anio,mes,municipio_id,municipio_nombre,concepto,monto);
+-----------------------------------------------------------------------------------------------------------------------------------------------
