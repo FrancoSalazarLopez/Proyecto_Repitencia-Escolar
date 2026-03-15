7979c3b00be5394ac70de70a732975960ebb45b7
Franco Salazar <francosalazarhr@gmail.com>
2026-03-15 01:01:49 -0300

    Update 02_load_data.sql

--- sql/02_load_data.sql
+++ sql/02_load_data.sql
@@ -7,6 +7,27 @@ INSERT INTO zona (zona)
     ("interior");
 
 ----------------------------------------------------------------------------------------------------------------------------------------------------
+INSERT INTO stg_localizacion_municipios (municipio_id,municipio_nombre,id_zona)
+VALUES
+-- Zona 1: Sur
+    (6028, 'Almirante Brown', 1), (6035, 'Avellaneda', 1), (6658, 'Quilmes', 1), (6434, 'Lanus', 1), 
+    (6287, 'Florencio Varela', 1), (6091, 'Berazategui', 1), (6490, 'Lomas de Zamora', 1), (6260, 'Esteban Echeverria', 1), 
+    (6274, 'Ezeiza', 1),
+    -- Zona 2: Oeste 
+    (6410, 'Ituzaingo', 2), (6568, 'Moron', 2), (6408, 'Hurlingham', 2), 
+    (6539, 'Merlo', 2), (6560, 'Moreno', 2),
+    -- Zona 3: Norte
+    (6742, 'San Isidro', 3), (6847, 'Vicente Lopez', 3), (6371, 'General San Martin', 3), 
+    (6762, 'San Miguel', 3), (6515, 'Malvinas Argentinas', 3), (6734, 'San Fernando', 3), (6805, 'Tigre', 3),
+    -- Zona 4: La Matanza 
+    (6427, 'La Matanza', 4),
+    -- Zona 5: Resto de los 40 municipios del AMBA 
+    (6098, 'Berisso', 5), (6119, 'Brandsen', 5), (6126, 'Campana', 5), (6134, 'Cañuelas', 5), (6245, 'Ensenada', 5), (6252, 'Escobar', 5), 
+    (6266, 'Exaltación de la Cruz', 5), (6329, 'General Las Heras', 5), (6364, 'General Rodríguez', 5), (6412, 'José C. Paz', 5), 
+    (6441, 'La Plata', 5), (6497, 'Luján', 5), (6525, 'Marcos Paz', 5), (6622, 'Pilar', 5), (6636, 'Presidente Perón', 5), (6769, 'San Vicente', 5), 
+    (6822, 'Tres de Febrero', 5), (6854, 'Zárate', 5);
+
+----------------------------------------------------------------------------------------------------------------------------------------------------
 LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/stg_dolar.csv' 
 INTO TABLE stg_dolar
 FIELDS TERMINATED BY ',' 
